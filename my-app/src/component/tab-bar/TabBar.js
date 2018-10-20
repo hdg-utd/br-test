8c1b09002bccf0cb7e9af16955b59d8f45827dcf
EC2 Default User <ec2-user@ip-172-31-4-198.ec2.internal>
2018-10-20 23:25:43 +0000

    created the tab-bar component

---
+++ my-app/src/component/tab-bar/TabBar.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+
+import 'TabBarComponent.css'
+
+class TabBar extends Component{
+  render() {
+    return (
+      <div className="tab-bar">
+        <h1>Tab Bar</h1>
+      </div> 
+    )
+  }
+}
+
+export default TabBar
